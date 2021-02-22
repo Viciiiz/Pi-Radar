42ed6be4eb68f04cd7a3a5b880bccc79856df15c
Victor Andrianarivo Rakotondranoro <71042541+Viciiiz@users.noreply.github.com>
2021-02-22 14:10:45 -0600

    Update send_sms.py

--- comp164/send_sms.py
+++ comp164/send_sms.py
@@ -2,11 +2,11 @@
 from twilio.rest import Client
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("ACbee6f4d77120177a4cc01c411fb37ffc", "f301c85d9637130b1ddaf8afcb43f049")
+client = Client("<your SID>", "<your token>")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
-client.messages.create(to="+17733875605", 
-                       from_="+19543985646", 
-                       body="Motion detected: http://192.168.0.10/page.php")
+client.messages.create(to="<your phone number>", 
+                       from_="<phone of sender>", 
+                       body="Motion detected: http://192.168.0.10/page.php") #link to web server
